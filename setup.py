eae7da4b30fb94eadb6aa3dab1d194c376697a04
Joe Brown <brwnjm@gmail.com>
2018-02-28 13:24:12 -0800

    gets version from init; updates url

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
+import io
+from os.path import dirname, join
 from setuptools import setup, find_packages
+
+
+def get_version(relpath):
+  """Read version info from a file without importing it"""
+  for line in io.open(join(dirname(__file__), relpath), encoding="cp437"):
+    if "__version__" in line:
+      if '"' in line:
+        # __version__ = "0.9"
+        return line.split('"')[1]
+      elif "'" in line:
+        return line.split("'")[1]
 
 
 with open('README.md') as f:
@@ -14,12 +27,12 @@ pkgs = find_packages(exclude=('examples'))
 
 setup(
     name='cmaputil',
-    version='1.0',
+    version=get_version("cmaputil/__init__.py"),
     description='Colormap analysis module.',
     long_description=readme,
     author='Jamie R. Nunez',
     author_email='jamie.nunez@pnnl.gov',
-    url='https://github.com/smcolby/cmaputil',
+    url='https://github.com/pnnl/cmaputil',
     license=license,
     packages=pkgs,
     install_requires=required
